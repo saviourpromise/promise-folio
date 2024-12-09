ad38fb5137c8259e8191856bf69fd5ba4f7c0001
PromiseNotNull <146076547+saviourpromise@users.noreply.github.com>
2024-12-09 09:22:50 +0100

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,17 +31,17 @@ export const metadata: Metadata = {
     title: "Saviour Promise — Frontend Developer",
     description:
     "Frontend Developer at CodSoft Technologies, passionate about crafting next-gen software. I drive growth by crafting user experiences that blend aesthetics with functionality through my development skills. I thrive on turning ideas into seamless digital solutions.",
-    url: "https://www.promise-folio.vercel.app",
-    siteName: "www.adeolabadero.vercel.app",
+    url: "https://promise-folio.vercel.app",
+    siteName: "https://promise-folio.vercel.app",
     images: [
       {
-        url: "https://i.ibb.co/FKMqc28/adeola-badero.png",
+        url: "https://media.licdn.com/dms/image/v2/D4D22AQFEwyOFqavOZw/feedshare-shrink_1280/feedshare-shrink_1280/0/1728748262401?e=1736380800&v=beta&t=2HT7HkK6jtL0LlJGMWiA9o_DtuVpy24eL7G6gFUW00c",
         width: 1200,
         height: 630,
         alt: "Saviour Promise — Frontend Developer",
       },
       {
-        url: "https://i.ibb.co/Y8hBTR4/ade-800.png",
+        url: "https://media.licdn.com/dms/image/v2/D4D22AQFX977sVGIDOg/feedshare-shrink_1280/feedshare-shrink_1280/0/1728748263345?e=1736380800&v=beta&t=DM-_cZA4_gb92UVS0DaRXOelyTSQ_41tfjp7-UIyuxY",
         width: 800,
         height: 800,
         alt: "Saviour Promise — Frontend Developer",
@@ -56,7 +56,7 @@ export const metadata: Metadata = {
     description:
       "Frontend Developer at CodSoft Technologies, passionate about crafting next-gen software. I drive growth by crafting user experiences that blend aesthetics with functionality through my development skills. I thrive on turning ideas into seamless digital solutions.",
     creator: "@promiseNotNull",
-    images: ["https://i.ibb.co/FKMqc28/adeola-badero.png"],
+    images: ["https://media.licdn.com/dms/image/v2/D4D22AQFxDJSGmjBcaA/feedshare-shrink_1280/feedshare-shrink_1280/0/1728748276855?e=1736380800&v=beta&t=ZgEXq40fZ0GbsZc9HGmumdF0gBpnnv2sW7gg5XTM4jo"],
   },
   robots: {
     index: true,
